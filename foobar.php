f75dd15a49bd4f9c96a4d97d81859d8283ac19a8
Engr. Nayab Bukhari, Syed <bec_dir@yahoo.com>
2019-06-25 22:34:30 +0300

    final foobar
    final foobar task PHP code

--- foobar.php
+++ foobar.php
@@ -9,9 +9,19 @@
  *
  **/
 
-foreach(range(1,100)as$n){
+echo
+"////////////////////////////////////////////////////////////////////////////////
+//• Output the numbers from 1 to 100                                           //
+//• Where the number is divisible by three (3) output the word 'foo'           //
+//• Where the number is divisible by five (5) output the word 'bar'            //
+//• Where the number is divisible by three (3) and (5) output the word 'foobar'//
+//• Only be a single PHP file                                                  //
+/////////////////////////////////////////////////////////////////////////////////\n\n";
+
+foreach(range(1,100) as $n){
    echo(''==($s=($n%3==0?"foo":"").($n%5==0?"bar":""))?$n:$s).', ';
 }
+
 echo "\n";
 
 ?>
